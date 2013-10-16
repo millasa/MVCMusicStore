dea70c0795ddb8a1afaf1d14a0c3b36517ebe812
unknown <melena.art@gmail.com>
2013-10-17 00:15:34 +0400

    initial commit 2

---
+++ MvcMusicStore/Controllers/StoreController.cs
@@ -0,0 +1,43 @@
+﻿using MvcMusicStore.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMusicStore.Controllers
+{
+    public class StoreController : Controller
+    {
+        //
+        // GET: /Store/
+        public ActionResult Index()
+        {
+            // Create a list of genres
+            var genres = new List<string> { "Rock", "Jazz", "Country", "Pop", "Disco" };
+            // Create our view model
+            var viewModel = new StoreIndexViewModel
+            {
+                NumberOfGenres = genres.Count(),
+                Genres = genres
+            };
+            return View(viewModel);
+        }
+
+        //
+        // GET: /Store/Browse/
+        public string Browse()
+        {
+            string message = "Store.Browse, Genre = " + Server.HtmlEncode(Request.QueryString["genre"]);
+            return Server.HtmlEncode(message);
+        }
+
+        //
+        // GET: /Store/Details/5
+        public string Details(int id=0)
+        {
+            string message = "Store.Details, ID = " + id;
+            return Server.HtmlEncode(message);
+        }
+    }
+}
