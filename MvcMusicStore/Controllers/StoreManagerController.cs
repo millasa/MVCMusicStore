9e99805dbedd3e38b904acfe653618f192da5026
unknown <melena.art@gmail.com>
2013-11-11 00:37:50 +0400

    fix edit post action

--- MvcMusicStore/Controllers/StoreManagerController.cs
+++ MvcMusicStore/Controllers/StoreManagerController.cs
@@ -79,10 +79,11 @@ namespace MvcMusicStore.Controllers
         // POST: /StoreManager/Edit/5
 
         [HttpPost]
-        public ActionResult Edit(Album album)
+        public ActionResult Edit(int id, Album album)
         {
             if (ModelState.IsValid)
             {
+                album.AlbumId = id;
                 db.Entry(album).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
